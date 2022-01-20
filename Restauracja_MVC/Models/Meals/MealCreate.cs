883ea65748849a70cf5547844d74e24067225ec2
Patryk Laptop <Patryk.Zub@interia.pl>
2022-01-20 11:17:06 +0100

    Changed models for Meals, TODO: select for category insted of input type text

--- Restauracja_MVC/Models/Meals/MealCreate.cs
+++ Restauracja_MVC/Models/Meals/MealCreate.cs
@@ -1,13 +1,12 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Restauracja_MVC.Models.Meals
 {
-    public class Meal
+    public class MealCreate 
     {
         [DisplayName("Nazwa")]
         [Required(ErrorMessage = "Pole nazwa jest wymagane")]
@@ -15,14 +14,17 @@ namespace Restauracja_MVC.Models.Meals
 
         [DisplayName("Cena")]
         [Required(ErrorMessage = "Pole cena jest wymagane")]
-        [Range(0.001, float.MaxValue, ErrorMessage = "Wartość pola {0} powinna być większa od {1}")]
+        [Range(0.001, 500.001, ErrorMessage = "Wartość pola {0} powinna być większa od {1}")]
         public float Price { get; set; }
-        
+
         [DisplayName("Opis")]
         [Required(ErrorMessage = "Pole opis jest wymagane")]
         public string Description { get; set; }
 
         [DisplayName("Kategoria")]
         public Byte Category { get; set; }
+        
+
+        public List<SelectListItem> MealsCategories { get; set; }
     }
 }
