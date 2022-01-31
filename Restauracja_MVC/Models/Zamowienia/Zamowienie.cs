75721de5193465dfd46ec19f7df32c0fda662d1b
Patryk PC <Patryk.Zub@interia.pl>
2022-01-31 22:55:46 +0100

    Home pange

--- Restauracja_MVC/Models/Zamowienia/Zamowienie.cs
+++ Restauracja_MVC/Models/Zamowienia/Zamowienie.cs
@@ -21,24 +21,21 @@ namespace Restauracja_MVC.Models.Zamowienia
         [RegularExpression(@"^[A-Z][a-zA-Z]*$", ErrorMessage = "Nazwisko musi zaczynać się z dużej litery oraz zawierać tylko litery")]
         public String SurnameOfUser { get; set; }
         [Required]
-        [Range(1,3)]
+        [Range(1, 3)]
         public int CityIDx { get; set; }
         [Required]
         public String Addressx { get; set; }
         [Required]
         [Phone]
         [StringLength(9, ErrorMessage = "Wprowadz poprawny numer telefonu")]
-        [Range(000000000,999999999)]
         public String Phonex { get; set; }
         [Required]
         public float Pricex { get; set; }
         [Required]
         public string Descriptionx { get; set; }
         [Required]
-        [StringLength(2)]
         public string Categoryx { get; set; }
         [Required]
-        [Range(0,10, ErrorMessage = "10 to maksymalna ilosc zamówionego dania")]
         public int Amountx { get; set; }
     }
 }
