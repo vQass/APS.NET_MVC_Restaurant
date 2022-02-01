c4cbe39d0b82b04a9b253f288c7a1f9292f6fa41
Piotr Nocun <piotrnocun0704@gmail.com>
2022-02-01 11:27:36 +0100

    Poprawa zamówień
    Poprawa zamówień

--- Restauracja_MVC/ViewModels/zamowienieViewModel.cs
+++ Restauracja_MVC/ViewModels/zamowienieViewModel.cs
@@ -9,10 +9,8 @@ namespace Restauracja_MVC.ViewModels
     public class zamowienieViewModel
     {
         [Required]
-        [RegularExpression(@"^[A-Z][a-zA-Z]*$", ErrorMessage = "Imię musi zaczynać się z dużej litery oraz zawierać tylko litery")]
         public String NameOfUser { get; set; }
         [Required]
-        [RegularExpression(@"^[A-Z][a-zA-Z]*$", ErrorMessage = "Nazwisko musi zaczynać się z dużej litery oraz zawierać tylko litery")]
         public String SurnameOfUser { get; set; }
         [Required]
         public int CityIDx { get; set; }
